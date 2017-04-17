9fe6170b7757b92d35142621beecb951575bc406
Roman Senn <roman.l.senn@gmail.com>
2017-04-17 04:26:18 +0200

    ...

--- SerialTest.pro
+++ SerialTest.pro
@@ -10,6 +10,14 @@ SOURCES = \
   Source/serial/src/serial.cc \
   Source/serialib/serialib.cpp
 
+HEADERS = \
+  Source/serial/include/serial/impl/unix.h \
+  Source/serial/include/serial/impl/win.h \
+  Source/serial/include/serial/serial.h \
+  Source/serial/include/serial/v8stdint.h \
+  Source/serialib/serialib.h
+
+
 #SOURCES +=  Source/serial/src/impl/list_ports/list_ports_linux.cc  Source/serial/src/impl/list_ports/list_ports_osx.cc  Source/serial/src/impl/list_ports/list_ports_win.cc
   
 LIBS_PRIVATE += -lgdi32 -lcomdlg32 -lsetupapi
--- Source/SerialTest.cpp
+++ Source/SerialTest.cpp
@@ -3,19 +3,60 @@
 
 #include "serial/include/serial/serial.h"
 #include <iostream>
+#include <windows.h>
 
 using namespace serial;
 
+Serial sport("COM1");
+
+uint64_t now() {
+    static LARGE_INTEGER s_frequency;
+    static bool s_use_qpc = QueryPerformanceFrequency(&s_frequency);
+    if (s_use_qpc) {
+        LARGE_INTEGER now;
+        QueryPerformanceCounter(&now);
+        return (1000LL * now.QuadPart) / s_frequency.QuadPart;
+    } else {
+        return GetTickCount();
+    }
+}
+
+void
+printCTS() {
+    static int prevCTS = -2;
+
+    int CTS = (int)sport.getCTS();
+
+    if(prevCTS != CTS) {
+
+        std::cout << "CTS = " << CTS << std::endl;
+        prevCTS = CTS;
+    }
+}
+
 int main(int argc, char *argv[]) {
 
-  Serial sport("COM1");
 
   if(!sport.isOpen()) {
   
     std::cerr << "Port not open" << std::endl;
     return 1;
   }
+  sport.setRTS(true);
+    uint64_t done = now() + 2000;
 
+    while(now() < done) {
+
+      sport.write((const uint8_t*)"\x00\xff\xff\xff\xff\xff\xff\xff", 1);
+      //sport.flushOutput();
+    }
+
+
+  for(;;) {
+    printCTS();
+
+    sport.waitForChange();
+  }
 
 return 0;
 }
